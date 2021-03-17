beb32fa6f33771a24858f0da24b0de6cb51fe5d6
Sujay Torvi <53830055+sujaytorvi@users.noreply.github.com>
2021-03-18 00:16:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import tensorflow as tf
 with zipfile.ZipFile('bayesian_nn.zip', 'r') as zip_ref:
     zip_ref.extractall('bayesian_nn')
 
-model = keras.models.load_model('/content/bayesian_nn')
+model = keras.models.load_model('/bayesian_nn')
 
 # hardcoding some inputs: please modify the variable to take values from the flutter app
 '''
